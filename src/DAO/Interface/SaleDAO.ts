dba165491690a9d67ce5884c58f3545b6c540938
fnagahashi <fabi.nagahashi@gmail.com>
2025-11-23 20:16:22 -0300

    feat: creat saleDAO

---
+++ src/DAO/Interface/SaleDAO.ts
@@ -0,0 +1,89 @@
+import IDAO from "../IDAO";
+import { DataSource, Repository } from "typeorm";
+import Sale from "../../entities/sale";
+
+export default class SaleDAO implements IDAO<Sale> {
+    private repository: Repository<Sale>;
+
+    constructor(dataSource: DataSource) {
+        this.repository = dataSource.getRepository(Sale);
+    }
+
+    async create(sale: Sale): Promise<Sale> {
+        return await this.repository.save(sale);
+    }
+
+    async update(sale: Sale): Promise<Sale> {
+        const saleExistente = await this.repository.findOne({ where: { id: sale.id } });
+        if (!saleExistente) {
+            throw new Error('Promoção não encontrada');
+        }
+
+        const saleAtualizado = this.repository.merge(saleExistente, sale);
+        return await this.repository.save(saleAtualizado);
+    }
+
+    async list(sale: Sale, operacao: string): Promise<Sale[]> {
+        switch(operacao) {
+            case 'buscarPorId':
+                return await this.repository.find({ where: { id: sale.id } });
+            
+            case 'buscarPorCodigo':
+                return await this.repository.find({ 
+                    where: { 
+                        codigoSale: sale.codigoSale, 
+                    } 
+                });
+            
+            case 'buscarAtivas':
+                return await this.repository.find({ 
+                    where: { 
+                        isActive: true,
+                        validoAte: new Date()
+                    } 
+                });
+            
+            case 'buscarTodas':
+                return await this.repository.find();
+            
+            default:
+                throw new Error('Operação não encontrada no SaleDAO');
+        }
+    }
+
+    async delete(sale: Sale): Promise<void> {
+        await this.repository.softDelete(sale.id);
+    }
+
+    async buscarPorCodigoAtivo(codigo: string): Promise<Sale | null> {
+        return await this.repository.findOne({
+            where: {
+                codigoSale: codigo,
+                isActive: true,
+                validoAte: new Date()
+            }
+        });
+    }
+
+    async desativarPromocao(id: string): Promise<Sale> {
+        const sale = await this.repository.findOne({ where: { id } });
+        if (!sale) {
+            throw new Error('Promoção não encontrada');
+        }
+
+        sale.isActive = false;
+        return await this.repository.save(sale);
+    }
+
+    async listarPromocoesAtivas(): Promise<Sale[]> {
+        return await this.repository.find({
+            where: {
+                isActive: true,
+                validoAte: new Date()
+            },
+            order: {
+                validoAte: 'ASC'
+            }
+        });
+    }
+}
