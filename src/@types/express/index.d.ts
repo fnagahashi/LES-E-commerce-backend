cf919fd472397ab73558cf3a316811812ab17cbc
fnagahashi <fabi.nagahashi@gmail.com>
2026-04-04 22:02:14 -0300

    fix: middleware

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,9 +1,14 @@
-declare namespace Express {
-  export interface Request {
-    user?: {
-      id: string;
-      email: string;
-      role: string;
-    };
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: string;
+      };
+    }
   }
 }
+export {};
