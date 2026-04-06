70bf72762a13edbb86157c17d67bdaaee8af6a68
fnagahashi <fabi.nagahashi@gmail.com>
2026-04-06 11:03:28 -0300

    fix: validationtotal value

--- src/strategy/payment/ValidationTotalValue.ts
+++ src/strategy/payment/ValidationTotalValue.ts
@@ -1,16 +1,25 @@
 import IStrategy from "../IStrategy";
 import Order from "../../entities/order";
+import { PaymentMethod } from "../../enum/PaymentMethod";
 
 export default class ValidationTotalValue implements IStrategy<Order> {
   executar(order: Order): Promise<string | undefined> {
     const errors: string[] = [];
 
     const totalValue = order.payment.reduce(
-      (soma, p) => soma +Number( p.paymentValue),
+      (soma, p) => soma + Number(p.paymentValue),
       0,
     );
+    const totalCoupons =
+      order.payment
+        ?.filter(
+          (p) =>
+            p.paymentMethod === PaymentMethod.cupomSale ||
+            p.paymentMethod === PaymentMethod.cupomExchange,
+        )
+        .reduce((acc, p) => acc + Number(p.paymentValue), 0) || 0;
 
-    if (totalValue < Number(order.totalPrice)) {
+    if (totalValue < Number(order.totalPrice) - totalCoupons) {
       errors.push(
         "O valor total dos pagamentos deve ser igual ao valor total da venda.",
       );
