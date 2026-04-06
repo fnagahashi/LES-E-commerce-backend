0c1dba877142c62ac19ec5ad89db74e228a6fd0c
fnagahashi <fabi.nagahashi@gmail.com>
2026-04-06 12:50:40 -0300

    fix:  migrations

--- src/database/migrations/1775486473450-auto-migration.ts
+++ src/database/migrations/1775486473450-auto-migration.ts
@@ -1,14 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AutoMigration1775440095548 implements MigrationInterface {
-    name = 'AutoMigration1775440095548'
+export class AutoMigration1775486473450 implements MigrationInterface {
+    name = 'AutoMigration1775486473450'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "cupom" DROP COLUMN "cupomValue"`);
         await queryRunner.query(`ALTER TABLE "cupom" ADD "cupomValue" numeric(10,2) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "creditCard" ALTER COLUMN "cardHolderName" SET NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "creditCard" ALTER COLUMN "cardHolderName" DROP NOT NULL`);
         await queryRunner.query(`ALTER TABLE "cupom" DROP COLUMN "cupomValue"`);
         await queryRunner.query(`ALTER TABLE "cupom" ADD "cupomValue" integer NOT NULL`);
     }
