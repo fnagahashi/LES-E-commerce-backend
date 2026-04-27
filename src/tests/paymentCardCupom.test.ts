0b66e2b54f5c8634bb5a83a72f2602418da34de2
fnagahashi <fabi.nagahashi@gmail.com>
2026-04-27 16:31:51 -0300

    feat: test with credit card + coupon

---
+++ src/tests/paymentCardCupom.test.ts
@@ -0,0 +1,83 @@
+import request from "supertest";
+import app from "../server";
+import { AppDataSource } from "../database";
+
+describe("Registro de Pedido - Pagamento com cartão + cupom", () => {
+  let token: string;
+  beforeAll(async () => {
+    process.env.NODE_ENV = "test";
+
+    if (!AppDataSource.isInitialized) {
+      await AppDataSource.initialize();
+    }
+
+    const loginResponse = await request(app).post("/api/login").send({
+      email: "barbara_dossantos@parker.com",
+      password: "Ba12345678*",
+    });
+
+    token = loginResponse.body.token;
+  });
+
+  afterAll(async () => {
+    await AppDataSource.destroy();
+  });
+
+  test("deve registrar pedido com pagamento via cartão + cupom", async () => {
+  const response = await request(app)
+    .post("/api/orders")
+    .set("Authorization", `Bearer ${token}`)
+    .send({
+      clientId: {
+        id: "be561743-1d30-4e1f-8d3c-ba9694ee90ae",
+      },
+
+      orderItems: [
+        {
+          bookId: "88e55e63-58fa-4ce0-9ce1-d5642af8c3cd",
+          quantity: 1,
+          unitaryValue: "52.90",
+          totalItemValue: "52.90",
+        },
+        {
+          bookId: "a5509d57-5130-4073-a169-7f88d2537f2f",
+          quantity: 2,
+          unitaryValue: "44.90",
+          totalItemValue: "89.80",
+        },
+      ],
+
+      payments: [
+        {
+          paymentMethod: "cupom_sale",
+          paymentValue: "50.00",
+          paymentStatus: "APPROVED",
+          cupomId: "0f40d192-3443-4668-b0db-ca5e552fcc06",
+        },
+        {
+          paymentMethod: "CREDIT_CARD",
+          paymentValue: "97.70",
+          paymentStatus: "APPROVED",
+          creditCard: {
+            id: "f093a981-38c9-47f2-b18f-50ce88e1ec0c",
+          },
+        },
+      ],
+
+      delivery: {
+        address: {
+          id: "9b4356b7-692a-4e1d-8774-00250eab1d00",
+        },
+        freightType: "STANDARD",
+        freightValue: "0",
+      },
+
+      orderDate: "2025-10-06",
+      totalPrice: "147.70",
+    });
+
+  expect(response.status).toBe(201);
+  expect(response.body.success).toBe(true);
+  expect(response.body.order.payment.length).toBe(2);
+});
+});
